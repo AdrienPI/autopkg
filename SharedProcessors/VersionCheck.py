fec5e6cae7b32ce6d895463a48a007368bf0084f
AdrienPI <82602400+AdrienPI@users.noreply.github.com>
2024-02-22 10:52:24 +0100

    Update VersionCheck.py

--- SharedProcessors/VersionCheck.py
+++ SharedProcessors/VersionCheck.py
@@ -12,10 +12,6 @@ class VersionCheck(Processor):
             "required": True,
             "description": "The version of the package to check."
         },
-        "app_name": {
-            "required": True,
-            "description": "The name of the application package."
-        },
         "directory": {
             "required": True,
             "description": "The directory to check for the existence of the package version."
@@ -29,20 +25,19 @@ class VersionCheck(Processor):
 
     def main(self):
         version = self.env.get("version")
-        app_name = self.env.get("app_name")
         directory = self.env.get("directory")
 
         # Check if the specified version exists in the directory
         for filename in os.listdir(directory):
-            if filename.endswith('.pkg') and app_name in filename:
+            if filename.endswith('.pkg'):
                 pkg_version = filename.split('-')[-1].split('.')[0]
                 if pkg_version == version:
                     self.env["version_exists"] = True
-                    self.output(f"Version {version} of {app_name} is already downloaded.")
+                    self.output(f"Version {version} is already downloaded.")
                     return
         # Version not found
         self.env["version_exists"] = False
-        self.output(f"Version {version} of {app_name} is not yet downloaded.")
+        self.output(f"Version {version} is not yet downloaded.")
 
 
 if __name__ == "__main__":
