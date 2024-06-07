85ea979ddcd25d77afef5c81d552d4226b6531c8
AdrienPI <adrien.pichard@edu-vd.ch>
2024-06-07 10:37:47 +0200

    add csvwriter for title editor

---
+++ SharedProcessors/CSVWriter_titleeditor.py
@@ -0,0 +1,73 @@
+#!/usr/local/autopkg/python
+
+import csv
+import os
+import subprocess
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = ["CSVWriter"]
+
+
+class CSVWriter(Processor):
+    """Writes app version and category to a CSV file."""
+
+    input_variables = {
+        "app_name": {
+            "required": True,
+            "description": "The app name."
+        },
+        "app_version": {
+            "required": True,
+            "description": "The app version."
+        },
+        "category": {
+            "required": True,
+            "description": "The app category."
+        },
+        "softwaretitle_name": {
+            "required": True,
+            "description": "The softwaretitle app name."
+        },
+        "app_minimum_version": {
+            "required": False,
+            "description": "The minimum supported OS for the app, may not exist"
+        }
+        "bundle_identifier": {
+            "required": True,
+            "description": "Bundle identifier of the app"
+        }
+    }
+    output_variables = {
+        "CSVWriter_summary_result": {
+        "description": "App data to output"
+        }
+    }
+
+    def main(self):
+        app_name = self.env.get("app_name")
+        app_version = self.env.get("app_version")
+        category = self.env.get("category")
+        softwaretitle_name = self.env.get("softwaretitle_name")
+        from_patch_management = 1
+        loggedInUser = subprocess.check_output("(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')",shell=True).decode("utf-8").rstrip("\n")
+        csv_file = f"/Users/{loggedInUser}/Desktop/packagesupload/PKG_Catalog.csv"
+
+        with open(csv_file, mode="a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([app_name, app_version, bundle_identifier, app_minimum_version, category, softwaretitle_name, from_patch_management])
+            
+        self.env["CSVWriter_summary_result"] = {
+            "summary_text": "The following new items were downloaded:",
+            "data": {
+                "App_name": self.env["app_name"],
+                "App_version": self.env["app_version"],
+                "App_category": self.env["category"]
+                "App_minimum_version": self.env["app_minimum_version"]
+                "App_bundle_identifier": self.env["bundle_identifier"]
+            }
+        }
+
+if __name__ == "__main__":
+    PROCESSOR = CSVWriter()
+    PROCESSOR.execute_shell()
